a6b3af5be9fbfe16ca41650e97a72d91a64c6229
Márvio Bezerra <marvio.bezerra@hotmail.com>
2016-09-23 23:47:30 -0300

    Added data source helper for unit test

---
+++ XCommon/UnitTest/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace XCommon.UnitTest
+{
+	public class DataItem<TEntity>
+	{
+		public TEntity Item { get; set; }
+
+		public bool Valid { get; set; }
+
+		public string Message { get; set; }
+
+		public object[] Cast()
+		{
+			return new object[] { Item, Valid, Message };
+		}
+	}
+}
---
+++ XCommon/UnitTest/DataSourceBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XCommon.UnitTest
+{
+	public abstract class DataSourceBase<TBase, TEntity>
+		where TBase : DataSourceBase<TBase, TEntity>, new()
+	{
+		protected static readonly TBase Instance = new TBase();
+
+		private static List<DataItem<TEntity>> DataLoad { get; set; }
+
+		protected abstract List<DataItem<TEntity>> Load();
+
+		public static List<TEntity> Data
+		{
+			get
+			{
+				if (DataLoad == null)
+					DataLoad = Instance.Load();
+
+				return DataLoad.Select(c => c.Item).ToList();
+			}
+		}
+
+		public static IEnumerable<object[]> DataSource
+		{
+			get
+			{
+				if (DataLoad == null)
+					DataLoad = Instance.Load();
+
+				foreach (var item in DataLoad)
+				{
+					yield return item.Cast();
+				}
+			}
+		}
+	}
+}
