cf812821ab5faee6ed32602bdb11613b0677c527
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2016-09-29 19:47:59 -0300

    Added TicketManagerFake for unit tests

---
+++ XCommon/Application/Login/TicketManagerFake.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using XCommon.Extensions.Converters;
+using XCommon.Patterns.Repository.Executes;
+
+namespace XCommon.Application.Login
+{
+	public class TicketManagerFake : ITicketManager
+	{
+		public string Culture { get; set; }
+
+		public bool IsAuthenticated { get; set; }
+
+		public ExecuteUser User
+		{
+			get
+			{
+				var userKey = UserKey;
+
+				if (userKey == Guid.Empty)
+					return null;
+
+				return new ExecuteUser
+				{
+					Key = userKey,
+					Name = "Fake"
+				};
+			}
+		}
+
+		public Guid UserKey
+		{
+			get
+			{
+				Guid result = Guid.Empty;
+
+				if (!IsAuthenticated)
+					return result;
+
+				return 1.ToGuid();
+			}
+		}
+
+		public async Task SignInAsync(TicketEntity signUpTicket)
+		{
+			await Task.Yield();
+		}
+
+		public async Task SignOutAsync()
+		{
+			await Task.Yield();
+		}
+	}
+}
