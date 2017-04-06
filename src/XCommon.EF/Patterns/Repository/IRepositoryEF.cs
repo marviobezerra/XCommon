d274e6b80bf47577b2662e13876e2cbc0cb73ca0
Marvio Silverio <marvio.bezerra@hotmail.com>
2017-04-06 00:18:01 -0400

    Add save optition without execute on repository

--- src/XCommon.EF/Patterns/Repository/IRepositoryEF.cs
+++ src/XCommon.EF/Patterns/Repository/IRepositoryEF.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using XCommon.Patterns.Repository.Entity;
 using XCommon.Application.Executes;
+using XCommon.Patterns.Repository.Entity;
 
 namespace XCommon.Patterns.Repository
 {
-    public interface IRepositoryEF<TEntity, TFilter> : IRepository<TEntity, TFilter>
+	public interface IRepositoryEF<TEntity, TFilter> : IRepository<TEntity, TFilter>
         where TEntity : EntityBase, new()
         where TFilter : FilterBase, new()
     {
-        Task<Execute<TEntity>> SaveAsync(Execute<TEntity> execute, DbContext context);
-        Task<Execute<List<TEntity>>> SaveManyAsync(Execute<List<TEntity>> execute, DbContext context);
+		Task<Execute<TEntity>> SaveAsync(TEntity entity, DbContext context);
+		Task<Execute<List<TEntity>>> SaveManyAsync(List<TEntity> entities, DbContext context);
+		Task<Execute<TEntity>> SaveAsync(Execute<TEntity> execute, DbContext context);
+        Task<Execute<List<TEntity>>> SaveManyAsync(Execute<List<TEntity>> entities, DbContext context);
     }
 }
