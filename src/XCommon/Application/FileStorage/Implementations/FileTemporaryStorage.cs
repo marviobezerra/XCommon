3c3d65f123f23b73f78d3f1938b3aa4f5b9fd783
Marvio Silverio <marvio.bezerra@hotmail.com>
2017-05-26 03:26:23 -0400

    Some changes on the repository and filestorage

---
+++ src/XCommon/Application/FileStorage/Implementations/FileTemporaryStorage.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace XCommon.Application.FileStorage.Implementations
+{
+	public class FileTemporaryStorage : FileStorageLocal, IFileTemporaryStorage
+	{
+		public FileTemporaryStorage(string rootFolder) : base(rootFolder)
+		{
+		}
+
+		public bool ClearAll()
+		{
+			if (Directory.Exists(Root))
+			{
+				Directory.Delete(Root, true);
+			}
+
+			Directory.CreateDirectory(Root);
+			return true;
+		}
+	}
+}
