b0f9a5c50ad024cb92293b021f6bf178d1590301
Marvio Silverio <marvio_silverio@dealer-fx.com>
2018-08-29 12:06:00 -0400

    Update CI build

---
+++ appveyor-version.ps1
@@ -0,0 +1,12 @@
+$TAG = (git describe --tags --long  --match "v/*").split('/')[1]
+
+Write-Host $TAG
+$MAJOR = $TAG.split('.')[0]
+$MINOR = $TAG.split('.')[1].split('-')[0]
+$PATCH = $TAG.split('.')[2].split('-')[0]
+$TICK = $TAG.split('.')[2].split('-')[1]
+$HASH = $TAG.split('.')[2].split('-')[2]
+
+$env:SOURCE_TAG=$TAG
+$env:SOURCE_VERSION="$MAJOR.$MINOR.$PATCH.$TICK"
+$env:SOURCE_HASH=$HASH
